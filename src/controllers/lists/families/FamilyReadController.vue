79fdbfb2932c29ce34d247834a4d4c55f78f05c9
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-08-15 21:30:14 +0200

    1

--- src/controllers/lists/families/FamilyReadController.vue
+++ src/controllers/lists/families/FamilyReadController.vue
@@ -5,24 +5,60 @@
         :id="id"
         :templateOverview="templateListGrid"
         hideRelations
+        @initialLoadHappened="$emit('initialLoadHappened')"
       />
     </q-card>
+    <div
+      v-show="initialLoadHappened"
+    >
+
+      <div class="row  q-col-gutter-md">
+        <div class="col-xl-6 col-md-6 col-sm-12 col-xs-12">
+
+          <q-card class="q-mb-md">
+            <q-expansion-item
+              label="Family Memberships"
+              expand-separator
+              class="bordered-expansion-item"
+              @show="renderSection1=true"
+            >
+              <div class="q-pa-md">
+
+                <familyMembershipList
+                  v-if="renderSection1"
+                  :parentKeyValuePair="{
+                    key: 'user_id',
+                    value: +session.user.id
+                  }"
+
+                />
+              </div>
+            </q-expansion-item>
+          </q-card>
+
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import familyRead from 'src/views/lists/families/FamilyRead.vue'
+import FamilyMembershipList from "src/views/lists/family-memberships/FamilyMembershipList.vue";
 
 export default {
     name: 'Family-read-controller',
     components: {
+      FamilyMembershipList,
         familyRead,
     },
 
     data() {
         return {
+          initialLoadHappened: false,
+          renderSection1: false,
             id: +this.$route.params.rId,
-          templateListGrid: {
+            templateListGrid: {
               class: "q-pa-md q-col-gutter-md",
               cols: [
                 {
@@ -36,15 +72,15 @@ export default {
                     hideLabel: true,
                   },
                 },
-                {
-                  width: 3,
-                  dataPoint: {
-                    type: "function",
-                    function: (item) => `${item.email}`,
-                    label: "Email",
-                    // xOrientation: true,
-                  },
-                },
+                // {
+                //   width: 3,
+                //   dataPoint: {
+                //     type: "function",
+                //     function: (item) => `${item.email}`,
+                //     label: "Email",
+                //     // xOrientation: true,
+                //   },
+                // },
               ],
             }
         }
