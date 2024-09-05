685708de0ccb067864ca003d9bc6768bbd3dc000
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-09-05 20:22:56 +0200

    1

--- src/models/orm-api/Event.js
+++ src/models/orm-api/Event.js
@@ -6,170 +6,179 @@ import Attendance from 'src/models/orm-api/Attendance';
 import {FieldUsageTypes} from 'quicklists-vue-orm-ui'
 
 export default class Event extends MyBaseModel {
-    static entity = 'event';
-    static entityUrl = '/api/events';
-    static primaryKey = 'id';
-    static titleKey = 'name';
-    static entityName = 'Event';
+  static entity = 'event';
+  static entityUrl = '/api/events';
+  static primaryKey = 'id';
+  static titleKey = 'name';
+  static entityName = 'Event';
 
-    static fileUrlPrefix = `${import.meta.env.VITE_API_BACKEND_URL}/storage`;
+  static fileUrlPrefix = `${import.meta.env.VITE_API_BACKEND_URL}/storage`;
 
-    static openRecord(pVal, item, router){
-      router.push({
-        name: '/lists/events/:rId/:rName',
-        params: {
-          rId: pVal,
-          rName: pVal,
-        },
-      })
-    }
+  static openRecord(pVal, item, router) {
+    router.push({
+      name: '/lists/events/:rId/:rName',
+      params: {
+        rId: pVal,
+        rName: pVal,
+      },
+    })
+  }
 
-    static parentWithables = [
-        'school',
-        'creator',
-        'updater'
-    ];
+  static parentWithables = [
+    'school',
+    'creator',
+    'updater'
+  ];
 
-    static rules = {
-        readables: () => true,
-        readable: (item) => true,
-        editable: (item) => true,
-        creatable: () => true,
+  static rules = {
+    readables: () => true,
+    readable: (item) => true,
+    editable: (item) => true,
+    creatable: () => true,
+  };
+
+  static fieldsMetadata = {
+    'id': {},
+    'name': {},
+    'image': {
+      usageType: 'fileImageType'
+    },
+    'start_datetime': {
+      usageType: FieldUsageTypes.timeRangeStart(),
+    },
+    'end_datetime': {
+      usageType: FieldUsageTypes.timeRangeEnd(),
+    },
+    'school_id': {
+      linkablesRule: () => {
+        return {}
+      }
+    },
+    'creator_id': {
+      autoFill(item) {
+        const session = VueCookies.get('VITE_AUTH');
+        if (item.creator_id) {
+          return item.creator_id
+        } else {
+          return session.user.id
+        }
+      }
+    },
+    'updater_id': {
+      autoFill(item) {
+        const session = VueCookies.get('VITE_AUTH');
+        return session.user.id
+      }
+    },
+    'created_at': {
+      autoFill(item) {
+        if (item.created_at) {
+          return item.created_at
+        } else {
+          const currentTimestamp = new Date().toISOString().slice(0, 19).replace('T', ' ');
+          return currentTimestamp
+        }
+      }
+    },
+    'updated_at': {
+      autoFill(item) {
+        const currentTimestamp = new Date().toISOString().slice(0, 19).replace('T', ' ');
+        return currentTimestamp
+      }
+    },
+    'latitude': {
+      usageType: FieldUsageTypes.mapExtraGeoLocLat(),
+    },
+    'longitude': {
+      usageType: FieldUsageTypes.mapExtraGeoLocLong(),
+    },
+  };
+
+  static fields() {
+    return {
+      'id': this.attr('').nullable(),
+      'name': this.attr(''),
+      'image': this.attr(''),
+      'start_datetime': this.attr(''),
+      'end_datetime': this.attr(''),
+      'school_id': this.attr(''),
+      'creator_id': this.attr('').nullable(),
+      'updater_id': this.attr('').nullable(),
+      'created_at': this.attr('').nullable(),
+      'updated_at': this.attr('').nullable(),
+      'creator': this.belongsTo(User, 'creator_id'),
+      'school': this.belongsTo(School, 'school_id'),
+      'updater': this.belongsTo(User, 'updater_id'),
+      'attendances': this.hasMany(Attendance, 'event_id'),
+      'latitude': this.attr('').nullable(),
+      'longitude': this.attr('').nullable(),
     };
+  }
 
-    static fieldsMetadata = {
-        'id': {},
-        'name': {},
-        'image': {
-          usageType: 'fileImageType'
-        },
-        'start_datetime': {
-          usageType: FieldUsageTypes.timeRangeStart(),
-        },
-        'end_datetime': {
-          usageType: FieldUsageTypes.timeRangeEnd(),
-        },
-        'school_id': { linkablesRule: () => { return {} } },
-        'creator_id': {
-          autoFill(item){
-            const session = VueCookies.get('VITE_AUTH');
-            if (item.creator_id){
-              return item.creator_id
-            } else {
-              return session.user.id
-            }
-          }
-        },
-        'updater_id': {
-          autoFill(item){
-            const session = VueCookies.get('VITE_AUTH');
-            return session.user.id
-          }
-        },
-        'created_at': {
-          autoFill(item){
-            if (item.created_at){
-              return item.created_at
-            } else {
-              const currentTimestamp = new Date().toISOString().slice(0, 19).replace('T', ' ');
-              return currentTimestamp
-            }
-          }
-        },
-        'updated_at': {
-          autoFill(item){
-            const currentTimestamp = new Date().toISOString().slice(0, 19).replace('T', ' ');
-            return currentTimestamp
-          }
-        },
-        'latitude': {
-          usageType: FieldUsageTypes.mapExtraGeoLocLat(),
-        },
-        'longitude': {
-          usageType: FieldUsageTypes.mapExtraGeoLocLong(),
-        },
-    };
+  static templateListGrid = {
+    // Define templateListGrid
+  };
 
-    static fields() {
-        return {
-            'id': this.attr('').nullable(),
-            'name': this.attr(''),
-            'image': this.attr(''),
-            'start_datetime': this.attr(''),
-            'end_datetime': this.attr(''),
-            'school_id': this.attr(''),
-            'creator_id': this.attr('').nullable(),
-            'updater_id': this.attr('').nullable(),
-            'created_at': this.attr('').nullable(),
-            'updated_at': this.attr('').nullable(),
-            'creator': this.belongsTo(User, 'creator_id'),
-            'school': this.belongsTo(School, 'school_id'),
-            'updater': this.belongsTo(User, 'updater_id'),
-            'attendances': this.hasMany(Attendance, 'event_id'),
-            'latitude': this.attr('').nullable(),
-            'longitude': this.attr('').nullable(),
-        };
-    }
+  static templateOverview = {
+    // Define templateOverview
+  };
 
-    static templateListGrid = {
-        // Define templateListGrid
-    };
+  static FetchAll(relationships = [], flags = {}, moreHeaders = {}, options = {
+    page: 1,
+    limit: 15,
+    filters: {},
+    clearPrimaryModelOnly: false
+  }) {
+    return this.customSupabaseApiFetchAll(
+      `${this.baseUrl}${this.entityUrl}`,
+      [...this.parentWithables, ...relationships],
+      flags,
+      this.mergeHeaders(moreHeaders),
+      options,
+      this
+    );
+  }
 
-    static templateOverview = {
-        // Define templateOverview
-    };
+  static FetchById(id, relationships = [], flags = {}, moreHeaders = {}) {
+    return this.customSupabaseApiFetchById(
+      `${this.baseUrl}${this.entityUrl}`,
+      id,
+      [...this.parentWithables, ...relationships],
+      flags,
+      this.mergeHeaders(moreHeaders),
+      this
+    );
+  }
 
-    static FetchAll(relationships = [], flags = {}, moreHeaders = {}, options = { page: 1, limit: 15, filters: {}, clearPrimaryModelOnly: false }) {
-        return this.customSupabaseApiFetchAll(
-            `${this.baseUrl}${this.entityUrl}`,
-            [...this.parentWithables, ...relationships],
-            flags,
-            this.mergeHeaders(moreHeaders),
-            options,
-            this
-        );
-    }
+  static Store(entity, relationships = [], flags = {}, moreHeaders = {}) {
+    return this.customSupabaseApiStore(
+      `${this.baseUrl}${this.entityUrl}`,
+      entity,
+      [...this.parentWithables, ...relationships],
+      flags,
+      this.mergeHeaders(moreHeaders),
+      this
+    );
+  }
 
-    static FetchById(id, relationships = [], flags = {}, moreHeaders = {}) {
-        return this.customSupabaseApiFetchById(
-            `${this.baseUrl}${this.entityUrl}`,
-            id,
-            [...this.parentWithables, ...relationships],
-            flags,
-            this.mergeHeaders(moreHeaders),
-            this
-        );
-    }
+  static Update(entity, relationships = [], flags = {}, moreHeaders = {}) {
+    return this.customSupabaseApiUpdate(
+      `${this.baseUrl}${this.entityUrl}`,
+      entity,
+      [...this.parentWithables, ...relationships],
+      flags,
+      this.mergeHeaders(moreHeaders),
+      this
+    );
+  }
 
-    static Store(entity, relationships = [], flags = {}, moreHeaders = {}) {
-        return this.customSupabaseApiStore(
-            `${this.baseUrl}${this.entityUrl}`,
-            entity,
-            [...this.parentWithables, ...relationships],
-            flags,
-            this.mergeHeaders(moreHeaders),
-            this
-        );
-    }
-
-    static Update(entity, relationships = [], flags = {}, moreHeaders = {}) {
-        return this.customSupabaseApiUpdate(
-            `${this.baseUrl}${this.entityUrl}`,
-            entity,
-            [...this.parentWithables, ...relationships],
-            flags,
-            this.mergeHeaders(moreHeaders),
-            this
-        );
-    }
-
-    static Delete(entityId, flags = {}, moreHeaders = {}) {
-        return this.customSupabaseApiDelete(
-            `${this.baseUrl}${this.entityUrl}`,
-            entityId,
-            flags,
-            this.mergeHeaders(moreHeaders),
-            this
-        );
-    }
+  static Delete(entityId, flags = {}, moreHeaders = {}) {
+    return this.customSupabaseApiDelete(
+      `${this.baseUrl}${this.entityUrl}`,
+      entityId,
+      flags,
+      this.mergeHeaders(moreHeaders),
+      this
+    );
+  }
 }
