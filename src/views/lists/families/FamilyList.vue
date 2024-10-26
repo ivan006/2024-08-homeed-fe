db7bee1e5ad2b0b98f7ac8f9cac16ef35b47d91d
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-10-26 09:32:35 +0200

    1

--- src/views/lists/families/FamilyList.vue
+++ src/views/lists/families/FamilyList.vue
@@ -1,14 +1,15 @@
 <template>
 
-            <SuperTable
-                :showMap="true"
-                :model="superTableModel"
-                @clickRow="openRecord"
-                :displayMapField="false"
-                :parentKeyValuePair="parentKeyValuePair"
-                :fetchFlags="fetchFlags"
-                noBorder
-            />
+  <SuperTable
+    :showMap="true"
+    :model="superTableModel"
+    @clickRow="openRecord"
+    :displayMapField="false"
+    :parentKeyValuePair="parentKeyValuePair"
+    :fetchFlags="fetchFlags"
+    :allowedFilters="[]"
+    noBorder
+  />
 
 </template>
 
@@ -17,37 +18,37 @@ import { SuperTable } from 'quicklists-vue-orm-ui'
 import Family from 'src/models/orm-api/Family'
 
 export default {
-    name: 'Family-list',
-    components: {
-        SuperTable,
-    },
+  name: 'Family-list',
+  components: {
+    SuperTable,
+  },
 
-    props: {
-        parentKeyValuePair: {
-            type: Object,
-            default: () => ({})
-        },
-        fetchFlags: {
-            type: Object,
-            default: () => ({})
-        }
+  props: {
+    parentKeyValuePair: {
+      type: Object,
+      default: () => ({})
     },
+    fetchFlags: {
+      type: Object,
+      default: () => ({})
+    }
+  },
 
-    computed: {
-        superTableModel() {
-            return Family
-        },
+  computed: {
+    superTableModel() {
+      return Family
     },
-    methods: {
-        openRecord(pVal, item, router) {
-            router.push({
-                name: '/lists/families/:rId/:rName',
-                params: {
-                    rId: pVal,
-                    rName: pVal,
-                },
-            })
+  },
+  methods: {
+    openRecord(pVal, item, router) {
+      router.push({
+        name: '/lists/families/:rId/:rName',
+        params: {
+          rId: pVal,
+          rName: pVal,
         },
+      })
     },
+  },
 }
 </script>
